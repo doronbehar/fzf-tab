0465e430c70d79c3b366255cb8b6d12871c00637
Doron Behar <doron.behar@gmail.com>
2024-01-01 19:43:37 +0200

    Monkey patch issue #176

--- fzf-tab.zsh
+++ fzf-tab.zsh
@@ -106,7 +106,7 @@
 
   # must run with user options; don't move `emulate -L zsh` above this line
   (( $+builtins[fzf-tab-compcap-generate] )) && fzf-tab-compcap-generate -i
-  COLUMNS=500 _ftb__main_complete "$@" || ret=$?
+  _ftb__main_complete "$@" || ret=$?
   (( $+builtins[fzf-tab-compcap-generate] )) && fzf-tab-compcap-generate -o
 
   emulate -L zsh -o extended_glob
